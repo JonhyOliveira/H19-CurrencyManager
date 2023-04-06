dc969cada526a1628ddffc4075d905bc5dcdef0e
João Oliveira <joaooliv@Joaos-MacBook-Air.local>
2023-04-06 18:32:18 +0100

    started testing with RSpec

---
+++ rails_app/spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  it "can be created if valid" do
+    user = User.new
+    user.email = Faker::Internet.email
+    user.password = Faker::Internet.password
+
+    expect { user.save }.to change(User, :count)
+  end
+
+  context "is not valid" do
+    it "with an invalid email" do
+      user = User.new
+      user.email = "joao123@"
+      user.password = Faker::Internet.password
+
+      expect(user).to_not be_valid
+    end
+
+    it "with a small password" do
+      user = User.new
+      user.email = Faker::Internet.email
+      user.password = "1234"
+
+      expect(user).to_not be_valid
+    end
+  end
+
+  context "can not be created" do
+    it "if another one with the same email already exists" do
+      user = User.new
+      user.email = Faker::Internet.email
+      user.password = Faker::Internet.password
+
+      user.save
+
+      user_copy = User.new
+      user_copy.email = user.email
+      user_copy.password = Faker::Internet.password
+
+      expect { user.save }.to_not change(User, :count)
+    end
+  end
+end
