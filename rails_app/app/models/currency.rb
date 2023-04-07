6ab4dfbaab4d13956629806a9927c15af94e3be2
João Oliveira <joaooliv@Joaos-MacBook-Air.local>
2023-04-07 12:56:23 +0100

    added currency validation

--- rails_app/app/models/currency.rb
+++ rails_app/app/models/currency.rb
@@ -1,2 +1,12 @@
 class Currency < ApplicationRecord
+    validates :symbol, presence: true, length: { minimum: 1 }
+    validates :latest_exchange_rate, presence: true, numericality: { greater_than: 0 }
+
+    def symbol= val
+        if val.respond_to? :upcase
+            val = val.upcase
+        end
+
+        self[:symbol] = val
+    end
 end
--- rails_app/spec/models/currency_spec.rb
+++ rails_app/spec/models/currency_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "is valid with acceptable parameters" do
+		currency = Currency.new
+
+		currency.symbol = "X"
+		currency.latest_exchange_rate = 0.1
+
+		expect { currency.save }.to change(Currency, :count)
+
+	end
+
+	context "is not valid" do
+		it "with invalid symbol" do
+			currency = Currency.new
+
+			currency.symbol = nil
+			currency.latest_exchange_rate = 1
+
+			expect(currency).to_not be_valid
+
+			currency.symbol = ""
+			expect(currency).to_not be_valid
+		end
+
+		it "with invalid exchange rate" do
+			currency = Currency.new
+
+			currency.symbol = "V"
+			currency.latest_exchange_rate = nil
+
+			expect(currency).to_not be_valid
+
+			currency.latest_exchange_rate = 0
+
+			expect(currency).to_not be_valid
+		end
+	end
 end
