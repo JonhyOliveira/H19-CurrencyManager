f37beef475040458144b94a0798a4467f926cb5b
João Oliveira <joao.oliveira@hole19golf.com>
2023-04-13 16:21:21 +0100

    👷‍♂️🎉 New job to load the currencies history

---
+++ rails_app/app/jobs/load_currencies_history_job.rb
@@ -0,0 +1,23 @@
+class LoadCurrenciesHistoryJob < ApplicationJob
+  queue_as :default
+
+  def perform **options
+
+    options[:date_range] ||= (DateTime.now.to_date - 1.day - 1.week)..(DateTime.now.to_date - 1.day)
+
+    Currency.transaction do
+      # update record list
+      result = FreecurrencyApi.call endpoint: "historical", options: { date_from: options[:date_range].first, date_to: options[:date_range].last }
+
+      unless result.failure?
+        result.data.each do | date, records |
+          records.each do | currency_code, ex_rate |
+            record = CurrencyRecord.find_or_initialize_by record_date: date.to_date, code: currency_code
+            record.update latest_exchange_rate: ex_rate
+          end
+        end
+      end
+
+    end
+  end
+end
