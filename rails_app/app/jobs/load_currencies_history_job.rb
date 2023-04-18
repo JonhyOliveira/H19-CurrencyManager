9f2f29aa2fb96b3c3d30f3815cfe0a38708fe591
João Oliveira <joao.oliveira@hole19golf.com>
2023-04-18 17:10:24 +0100

    now uses an insert_all

--- rails_app/app/jobs/load_currencies_history_job.rb
+++ rails_app/app/jobs/load_currencies_history_job.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 class LoadCurrenciesHistoryJob < ApplicationJob
   queue_as :default
-
+  
   def perform **options
-
+    
     options[:date_range] ||= (DateTime.now.to_date - 1.day - 1.week)..(DateTime.now.to_date - 1.day)
-
+    
     puts options[:date_range]
-
-    Currency.transaction do
-      # update record list
-      result = FreecurrencyApi.call endpoint: "historical", options: { date_from: options[:date_range].first, date_to: options[:date_range].last }
-
-      unless result.failure?
-        result.data.each do |date, records|
-          records.each do |currency_code, ex_rate|
-            record = CurrencyRecord.find_or_initialize_by record_date: date.to_date, code: currency_code
-            record.update latest_exchange_rate: ex_rate
-          end
+    
+    # update record list
+    result = FreecurrencyApi.call endpoint: "historical", options: { date_from: options[:date_range].first, date_to: options[:date_range].last }
+    
+    unless result.failure?
+      # insert_all ignores conflicts by default (doesn't fail if there is a conflict)
+      CurrencyRecord.insert_all result.data.flat_map do |date, incoming_records| 
+        incoming_records.map do |currency_code, ex_rate|
+          { record_date: date.to_date, code: currency_code, latest_exchange_rate: ex_rate }
         end
       end
-
     end
+    
   end
+end
 end
