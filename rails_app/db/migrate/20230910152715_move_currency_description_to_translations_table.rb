3f75aa2128a07f3b837e3d5976698c820b7c2807
joaooliv <jfv.oliveira@campus.fct.unl.pt>
2023-09-10 16:42:11 +0100

    add down migration to data migration

--- rails_app/db/migrate/20230910152715_move_currency_description_to_translations_table.rb
+++ rails_app/db/migrate/20230910152715_move_currency_description_to_translations_table.rb
@@ -4,4 +4,10 @@ class MoveCurrencyDescriptionToTranslationsTable < ActiveRecord::Migration[7.0]
       Currency::Translation.create(currency_id: curr.id, locale: "en", description: curr.description_in_database)
     end
   end
+
+  def down
+    Currency::Translation.where(locale: "en").find_each do |curr_translation|
+      Currency.find(curr_translation.currency_id).update_column :description, curr_translation.description
+    end
+  end
 end
